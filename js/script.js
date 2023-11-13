26cf0c6185120485284d3cff8082d467958d0a1c
priyarakkan <priyadharsinikdp@gmail.com>
2023-11-13 13:10:47 -0800

    update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Nav
 const menu1 = document.querySelector('.menu');
 const hamburger1 = document.querySelector('.hamburger');
 
@@ -9,4 +10,52 @@ hamburger1.addEventListener('click', () => {
 menu1.addEventListener('click', () => {
     menu1.classList.remove('menuOpen');
     hamburger1.classList.remove('hamburgerOpen');
+})
+
+//SLIDE SHOW
+// const images = document.querySelectorAll('.slideshow img');
+// let currentIndex = 0;
+// images[currentIndex].classList.add('active');
+
+// setInterval(() => {
+//     images[currentIndex].classList.remove('active');
+//     currentIndex = (currentIndex + 1) % 4;
+//     images[currentIndex].classList.add('active');
+// }, 3000);
+
+const images = document.querySelectorAll('.slideshow img');
+let currentIndex = 0;
+
+function showImage(index) {
+  images[currentIndex].classList.remove('active');
+  images[currentIndex].style.opacity = 0;
+  images[index].classList.add('active');
+  images[index].style.opacity = 1;
+  currentIndex = index;
+}
+
+images.forEach((image, index) => {
+  image.style.opacity = 0;
+  image.addEventListener('load', () => {
+    if(index === 0) {
+      image.classList.add('active');
+      image.style.opacity = 1;
+    }
+  })
+})
+
+setInterval(() => {
+  let nextIndex = currentIndex + 1;
+  if(nextIndex >= images.length) {
+    nextIndex = 0;
+  }
+  showImage(nextIndex)
+}, 3000);
+
+//modal
+document.querySelectorAll('.imageModalContainer img').forEach(image => {
+    image.addEventListener('click', () => {
+        document.querySelector('.popupImage').computedStyleMap.display = 'block';
+        document.querySelector('.popupImage img').src = image.getAttribute('src');
+    })
 })
