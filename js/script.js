694bc41364a25702b9987dad85553abc2589ae05
priyarakkan <priyadharsinikdp@gmail.com>
2023-11-13 19:47:13 -0800

    update popmodal

--- js/script.js
+++ js/script.js
@@ -13,49 +13,21 @@ menu1.addEventListener('click', () => {
 })
 
 //SLIDE SHOW
-// const images = document.querySelectorAll('.slideshow img');
-// let currentIndex = 0;
-// images[currentIndex].classList.add('active');
-
-// setInterval(() => {
-//     images[currentIndex].classList.remove('active');
-//     currentIndex = (currentIndex + 1) % 4;
-//     images[currentIndex].classList.add('active');
-// }, 3000);
-
 const images = document.querySelectorAll('.slideshow img');
 let currentIndex = 0;
-
-function showImage(index) {
-  images[currentIndex].classList.remove('active');
-  images[currentIndex].style.opacity = 0;
-  images[index].classList.add('active');
-  images[index].style.opacity = 1;
-  currentIndex = index;
-}
-
-images.forEach((image, index) => {
-  image.style.opacity = 0;
-  image.addEventListener('load', () => {
-    if(index === 0) {
-      image.classList.add('active');
-      image.style.opacity = 1;
-    }
-  })
-})
+images[currentIndex].classList.add('active');
 
 setInterval(() => {
-  let nextIndex = currentIndex + 1;
-  if(nextIndex >= images.length) {
-    nextIndex = 0;
-  }
-  showImage(nextIndex)
+    images[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex + 1) % 4;
+    images[currentIndex].classList.add('active');
 }, 3000);
+
 
 //modal
 document.querySelectorAll('.imageModalContainer img').forEach(image => {
     image.addEventListener('click', () => {
-        document.querySelector('.popupImage').computedStyleMap.display = 'block';
+        document.querySelector('.popupImage').style.display = 'block';
         document.querySelector('.popupImage img').src = image.getAttribute('src');
     })
 });
