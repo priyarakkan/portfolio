93cee572c9ac53a1337acd16f7ac4ed87f6f73db
priyarakkan <priyadharsinikdp@gmail.com>
2023-11-14 18:16:31 -0800

    home page response update

--- js/script.js
+++ js/script.js
@@ -11,26 +11,3 @@ menu1.addEventListener('click', () => {
     menu1.classList.remove('menuOpen');
     hamburger1.classList.remove('hamburgerOpen');
 })
-
-//SLIDE SHOW
-const images = document.querySelectorAll('.slideshow img');
-let currentIndex = 0;
-images[currentIndex].classList.add('active');
-
-setInterval(() => {
-    images[currentIndex].classList.remove('active');
-    currentIndex = (currentIndex + 1) % 4;
-    images[currentIndex].classList.add('active');
-}, 3000);
-
-
-//modal
-document.querySelectorAll('.imageModalContainer img').forEach(image => {
-    image.addEventListener('click', () => {
-        document.querySelector('.popupImage').style.display = 'block';
-        document.querySelector('.popupImage img').src = image.getAttribute('src');
-    })
-});
-document.querySelector('.popupImage span').addEventListener('click', () => {
-    document.querySelector('.popupImage').style.display = 'none';
-});
---
+++ js/slideshow.js
@@ -0,0 +1,23 @@
+
+//SLIDE SHOW
+const images = document.querySelectorAll('.slideshow img');
+let currentIndex = 0;
+images[currentIndex].classList.add('active');
+
+setInterval(() => {
+    images[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex + 1) % 4;
+    images[currentIndex].classList.add('active');
+}, 3000);
+
+
+//modal
+document.querySelectorAll('.imageModalContainer img').forEach(image => {
+    image.addEventListener('click', () => {
+        document.querySelector('.popupImage').style.display = 'block';
+        document.querySelector('.popupImage img').src = image.getAttribute('src');
+    })
+});
+document.querySelector('.popupImage span').addEventListener('click', () => {
+    document.querySelector('.popupImage').style.display = 'none';
+});
