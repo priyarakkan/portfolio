eb329eab0251fa25d26e380b868e7bc79e8dbd47
priyarakkan <priyadharsinikdp@gmail.com>
2024-01-03 15:53:54 -0800

    updated

--- pages/example/frameWork/script/resteurantFramework.js
+++ pages/example/frameWork/script/resteurantFramework.js
@@ -18,82 +18,193 @@ submenuIcon.addEventListener('click', () =>{
 
  
 
+
+
+
+
 // ----------------- Carousel -----------------//
-const slideWrapper = document.querySelector('.carouselItems');
-const slides = Array.from(slideWrapper.children);
-  console.log(slides);
-const nextBtn = document.querySelector('.rightBtn');
-const prevBtn = document.querySelector('.leftBtn');
-const slideWidth = slides[0].getBoundingClientRect().width;
-console.log(slideWidth);
-
-// convert above to a named function
-const setSlidePosition = (slide, index) => {
-  slide.style.left = slideWidth * index + 'px';
-};
-
-slides.forEach(setSlidePosition);
-
-// Reusable functions ---
-const moveToSlide = (slideWrapper, currentSlide, targetSlide) => {
-  slideWrapper.style.transform =  'translateX(-' + targetSlide.style.left + ')';
-  currentSlide.classList.remove('currentSlide');
-  targetSlide.classList.add('currentSlide');
-}
-
-
-const hideShowArrows = (slides, prevBtn, nextBtn, targetIndex) => {
-  if(targetIndex === 0) {
-    prevBtn.classList.add('isHidden');
-    nextBtn.classList.remove('isHidden');
-  } else if (targetIndex === slides.length - 1) {
-    prevBtn.classList.remove('isHidden');
-    nextBtn.classList.add('isHidden');
-  } else {
+if(document.querySelector('.carouselItems')){
+  const slideWrapper = document.querySelector('.carouselItems');
+  const slides = Array.from(slideWrapper.children);
+  const nextBtn = document.querySelector('.carouselRightBtn');
+  const prevBtn = document.querySelector('.carouselLeftBtn');
+  const dotsNav = document.querySelector('.carouselNav');
+  const dots = Array.from(dotsNav.children);
+  
+  const slideWidth = slides[0].getBoundingClientRect().width;
+  
+  const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + 'px';
+  };
+  
+  slides.forEach(setSlidePosition);
+  
+  const moveToSlide = (currentSlide, targetSlide) => {
+    slideWrapper.style.transform =  'translateX(-' + targetSlide.style.left + ')';
+    currentSlide.classList.remove('currentSlide');
+    targetSlide.classList.add('currentSlide');
+  }
+  
+  const updateDots = (targetDot) => {
+    const currentDot = dotsNav.querySelector('.currentSlide');
+    currentDot.classList.remove('currentSlide');
+    targetDot.classList.add('currentSlide');
+  }
+  
+  const hideShowArrows = (targetIndex) => {
     prevBtn.classList.remove('isHidden');
     nextBtn.classList.remove('isHidden');
+    
+    if(targetIndex === 0 ) {
+      prevBtn.classList.add('isHidden');
+    }
+    else if (targetIndex === slides.length - 1) {
+      nextBtn.classList.add('isHidden');
+    }
   }
+  
+  var carouselIdle;
+  var carouselIndex = 1;
+  function rotateSlide() {
+    const currentSlide = slideWrapper.querySelector('.currentSlide');
+    if(currentSlide === slides[slides.length-1]){
+      moveToSlide(currentSlide, slides[0]);
+      updateDots(dots[0]);
+      hideShowArrows(0);
+    }
+    else {
+      moveToSlide(currentSlide, slides[carouselIndex]);
+      updateDots(dots[carouselIndex]);
+      hideShowArrows(carouselIndex);
+     }
+    carouselIndex = (carouselIndex + 1) % slides.length;
+  }
+  
+  function startCarousel() {
+    carouselIdle = setInterval(rotateSlide, 5000); 
+  }
+  startCarousel();
+  
+  const pauseCarousel = _ => {
+    if(carouselIdle){
+      clearInterval(carouselIdle);
+      setTimeout(startCarousel, 10000);
+      carouselIdle = 0;
+    }
+    carouselIndex = slides.findIndex(slide => slide.classList.contains('currentSlide')) + 1;
+  }
+  
+  const changeSlide = (changedSlide, changedDot, currentSlide) => {
+    moveToSlide(currentSlide, changedSlide);
+    updateDots(changedDot);
+    hideShowArrows(slides.findIndex(slide => slide === changedSlide));
+    pauseCarousel();
+  }
+  
+  nextBtn.addEventListener('click', _ => {
+    const currentSlide = slideWrapper.querySelector('.currentSlide');
+    const nextSlide = currentSlide.nextElementSibling;
+    const nextDot = dotsNav.querySelector('.currentSlide').nextElementSibling;
+    changeSlide(nextSlide, nextDot, currentSlide);
+  })
+  
+  prevBtn.addEventListener('click', _ => {
+    const currentSlide = slideWrapper.querySelector('.currentSlide');
+    const prevSlide = currentSlide.previousElementSibling;
+    const prevDot = dotsNav.querySelector('.currentSlide').previousElementSibling;
+    changeSlide(prevSlide, prevDot, currentSlide);
+  })
+  
+  dotsNav.addEventListener('click', e => {
+    const targetDot = e.target.closest('button');
+    
+    if(!targetDot) return;
+    
+    const currentSlide = slideWrapper.querySelector('.currentSlide');
+    
+    const targetIndex = dots.findIndex(dot => dot === targetDot);
+    const targetSlide = slides[targetIndex];
+    
+    moveToSlide(currentSlide, targetSlide);
+    updateDots(targetDot);
+    hideShowArrows(targetIndex);
+    pauseCarousel();
+  })
+  }
+  function debounce(func, delay) {
+    let timerId;
+    return function() {
+      const context = this;
+      const args = arguments;
+      clearTimeout(timerId);
+      timerId = setTimeout(function() {
+        func.apply(context, args);
+      }, delay)
+    }
+  }
+  
+  window.addEventListener('resize', debounce(function() {
+    location.reload();
+  }, 250)); 
+
+  /************
+ * MODAL *
+ ************/
+const modal = document.querySelector('.modal');
+function openPopup(){
+    modal.classList.add('open-popup');
+}
+function closePopup(){
+    modal.classList.remove('open-popup');
 }
 
-// -- end reusbale functions
-
-// next button
-nextBtn.addEventListener('click', e => {
-  const currentSlide = slideWrapper.querySelector('.currentSlide');
-  const nextSlide = currentSlide.nextElementSibling;
-  const nextIndex = slides.findIndex(slide => slide === nextSlide);
-  moveToSlide(slideWrapper, currentSlide, nextSlide);
-  hideShowArrows(slides, prevBtn, nextBtn, nextIndex);
-});
-
-//previous button
-prevBtn.addEventListener('click', e => {
-  const currentSlide = slideWrapper.querySelector('.currentSlide');
-  const prevSlide = currentSlide.previousElementSibling;
-  const prevIndex = slides.findIndex(slide => slide === prevSlide);
-  moveToSlide(slideWrapper, currentSlide, prevSlide);
-  hideShowArrows(slides, prevBtn, nextBtn, prevIndex);
-});
-
-
-function debounce(func, delay) {
-  let timerId;
-  return function() {
-    const context = this;
-    const args = arguments;
-    clearTimeout(timerId);
-    timerId = setTimeout(function() {
-      func.apply(context, args);
-    }, delay);
-  }
-}
-// DEALING WITH RESIZE AND ENSURING PAGE LOOKS OK
-window.addEventListener('resize', debounce(function() {
-
-  // RELOAD OF THE PAGE
-  location.reload();
-
-},100))
-
-
-
+/************
+ *ORDER  APP*
+ ************/
+ const plates = document.querySelectorAll(".addItemButton");
+ const orderUp = document.querySelector("#orderUp");
+ var order = [];
+ var totalPrice = [];
+ 
+ plates.forEach(plate => plate.addEventListener('click', addPlateToOrder));
+ function addPlateToOrder(e){
+   if(!e.target.classList.contains("added")) {
+     order.push(e.target.id);
+     totalPrice.push(e.target.previousElementSibling.innerHTML);
+     e.target.value = "- Remove";
+     e.target.classList.add("added");
+   }
+   else { removePlateFromOrder(e); }
+ }
+ function removePlateFromOrder(e) {
+   order.splice(order.indexOf(e.target.id), 1);
+   totalPrice.splice(totalPrice.indexOf(e.target.previousElementSibling.innerHTML), 1);
+   e.target.value = "+ Add";
+   e.target.classList.remove("added");
+ }
+ 
+ orderUp.addEventListener('click', completeOrder);
+ 
+ function completeOrder(e) {
+   e.preventDefault();
+   const orderTotal = document.querySelector('#orderTotal');
+   let costTotal = 0;
+   totalPrice.forEach(cost => {
+     cost *= 100; //multiply to prevent floating point errors
+     costTotal += cost;
+   })
+   costTotal /= 100;
+   costTotal = costTotal.toFixed(2);
+   costTotal = '$' + costTotal;
+   orderTotal.textContent += costTotal
+   
+   const orderPopup = document.querySelector('#orderPopup');
+   const orderSummary = document.querySelector('#orderSummary');
+   order.forEach(order => { 
+     let node = document.createElement("li");
+     let textNode = document.createTextNode(document.querySelector('label[for="' + order +'"]').firstElementChild.textContent);
+     node.appendChild(textNode);
+     orderSummary.appendChild(node);
+   })
+ }
+  
